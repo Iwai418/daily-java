4b830f7447b5b14c98a73a5a32da55136f5ad796
edu <edu@AOS-N043.ARCHT1.local>
2025-11-13 17:36:25 +0900

    プロジェクトのプッシュ11/13分

--- morning_practice_for_req-train/src/day01/Lesson1.java
+++ morning_practice_for_req-train/src/day01/Lesson1.java
@@ -21,7 +21,42 @@ public class Lesson1 {
 
 	public static void main(String[] args) {
 		/*ここから記入*/
-
+		System.out.println("出力します");
+		
+		int num;
+		num = 12;
+		//numの値を出力
+		System.out.println(12);
+		
+		double d;
+		// s に1.6を代入
+		d =1.6;
+		//d の値を出力
+		System.out.println(1.6);
+		
+		String str;
+		str = "こんにちは";
+		//str の値を出力
+		System.out.println("こんにちは");
+		
+		boolean b;
+		//b に true　を入力
+		b = true;
+		//b の値を入力
+		System.out.println("true");
+		
+		int num;
+		num = 12;
+		double dnum = 1.6;
+		//helloに挨拶を代入する
+		String hello ="こんにちは";
+		boolean b;
+		b = true;
+		
+		System.out.println("出力します");
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(hello + "\n" + b);
+		System.out.println(b);
 	}
-
 }
